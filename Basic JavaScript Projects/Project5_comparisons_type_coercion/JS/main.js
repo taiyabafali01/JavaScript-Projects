c5766dc898c74a08aca47edb4df206331d2bddf2
taiyabafali01 <taiyabafali@gmail.com>
2025-11-29 20:54:39 -0600

    Add files via upload

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,14 @@
+// Triple Equal Signs Assignment
+// === compares both VALUE and DATA TYPE
+
+// ✔ Return true: same value + same data type
+console.log(10 === 10);  // true
+
+// ✔ Return false: different value + different data type
+console.log(5 === "20");  // false
+
+// ✔ Return false: same value + different data type
+console.log(7 === "7");  // false
+
+// ✔ Return false: same data type + different value
+console.log(15 === 10);  // false
